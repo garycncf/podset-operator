b782785400793e0206eefd6ce00d0735676079b3
garycncf <74400095+garycncf@users.noreply.github.com>
2021-09-04 23:16:49 +0800

    change package path

--- podset_controller.go
+++ podset_controller.go
@@ -25,7 +25,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
 
-	appv1alpha1 "github.com/redhat/podset-operator/api/v1alpha1"
+	appv1alpha1 "github.com/example/podset-operator/api/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
